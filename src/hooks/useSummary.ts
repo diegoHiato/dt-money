c1d6051a9c995c268b1abc000b313dc2aaacafc3
diegoHiato <diegofreitas_oliveira@proton.me>
2023-07-27 16:19:43 -0300

    Apply useMemo for better performance

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../contexts/Transactions/Context'
 
@@ -6,31 +7,33 @@ export const useSummary = () => {
     TransactionsContext,
     (context) => context.transactions,
   )
-  const summary = transactions.reduce(
-    (accumulator, transaction) => {
-      switch (transaction.type) {
-        case 'income':
-          accumulator.income += transaction.price
-          accumulator.outcome += transaction.price
-          break
+  const summary = useMemo(() => {
+    return transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.price
+            accumulator.outcome += transaction.price
+            break
 
-        case 'outgo':
-          accumulator.outgo += transaction.price
-          accumulator.outcome -= transaction.price
-          break
+          case 'outgo':
+            accumulator.outgo += transaction.price
+            accumulator.outcome -= transaction.price
+            break
 
-        default:
-          return accumulator
-      }
+          default:
+            return accumulator
+        }
 
-      return accumulator
-    },
-    {
-      income: 0,
-      outgo: 0,
-      outcome: 0,
-    },
-  )
+        return accumulator
+      },
+      {
+        income: 0,
+        outgo: 0,
+        outcome: 0,
+      },
+    )
+  }, [transactions])
 
   return summary
 }
